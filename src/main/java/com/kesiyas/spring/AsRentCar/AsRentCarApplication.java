35dbfee5df4d98525c5121c09cf6c2bbe336d288
DESKTOP-I633S8U\user <93star2@gmail.com>
2022-10-05 16:48:01 +0900

    [정민우] - 서버 연동 테스트 완료 - hello world 테스트 완료

--- src/main/java/com/kesiyas/spring/AsRentCar/AsRentCarApplication.java
+++ src/main/java/com/kesiyas/spring/AsRentCar/AsRentCarApplication.java
@@ -1,9 +1,12 @@
 package com.kesiyas.spring.AsRentCar;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class AsRentCarApplication {
 
 	public static void main(String[] args) {
