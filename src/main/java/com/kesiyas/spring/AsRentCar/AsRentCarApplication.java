3a869fec51734a83db7a7634a4266979c103389c
DESKTOP-I633S8U\user <93star2@gmail.com>
2022-10-05 16:36:13 +0900

    [정민우] - AsRentCar 프로젝트 구성

---
+++ src/main/java/com/kesiyas/spring/AsRentCar/AsRentCarApplication.java
@@ -0,0 +1,13 @@
+package com.kesiyas.spring.AsRentCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsRentCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsRentCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kesiyas/spring/AsRentCar/AsRentCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kesiyas.spring.AsRentCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsRentCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
