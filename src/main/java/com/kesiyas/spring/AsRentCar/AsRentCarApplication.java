c4fb78d6b667d5e31ca2cfea5b1f2ac84374b6ec
DESKTOP-I633S8U\user <93star2@gmail.com>
2022-10-05 18:23:47 +0900

    [정민우] - jsp 연동 완료

--- src/main/java/com/kesiyas/spring/AsRentCar/AsRentCarApplication.java
+++ src/main/java/com/kesiyas/spring/AsRentCar/AsRentCarApplication.java
@@ -1,12 +1,10 @@
 package com.kesiyas.spring.AsRentCar;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class AsRentCarApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/kesiyas/spring/AsRentCar/hello/HelloController.java
+++ src/main/java/com/kesiyas/spring/AsRentCar/hello/HelloController.java
@@ -1,16 +1,17 @@
 package com.kesiyas.spring.AsRentCar.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloController {
 
-	@RequestMapping("/hello")
-	@ResponseBody
+	@GetMapping("/view")
 	public String hello() {
-		return "Hello World!";
+		return "user/hello";
 	}
+	
 	
 }
